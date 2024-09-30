e79fcda95e05398a2a883c1bee5b401e7305b7d6
Astro <dev.astro@hotmail.com>
2024-09-30 23:21:42 +0100

    feat: removed built ignore from tracking

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/astro/Documents/another backup/projects/Projects/redocs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/astro/Documents/another backup/projects/Projects/redocs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/astro/Documents/another backup/projects/Projects/redocs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/astro/Documents/another backup/projects/Projects/redocs/src/css/custom.css"),
-];
