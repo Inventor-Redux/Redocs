290b67c5241436392fceffd47123a94fb649f1f1
Zammer <zammer.productions@gmail.com>
2024-10-01 19:40:13 +0000

    fix: Clear cache

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/workspaces/Redocs/node_modules/infima/dist/css/default/default.css"),
-  require("/workspaces/Redocs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/workspaces/Redocs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/workspaces/Redocs/src/css/custom.css"),
-];
