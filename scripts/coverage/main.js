e582fc621f1d0a7dbd2ff15044fadc6cccd17768
Astro <dev.astro@hotmail.com>
2024-10-29 20:05:31 +0000

    better fix?

--- scripts/coverage/main.js
+++ scripts/coverage/main.js
@@ -1,12 +1,6 @@
 const { writeFileSync } = require("fs");
 const { parseFileTree } = require("./tree");
-const readline = require("readline-sync");
-let go = readline.question("Calculate Coverage? y/n: ");
-while (go != "y" && go != "n") {
-  go = readline.question("Invalid Input, Calculate Coverage? y/n: ");
-}
-if (go === "y") {
+
+
   writeFileSync("static/coverage.json", JSON.stringify(parseFileTree("docs")))
-} else if (go === "n") {
-  return;
-}
+
